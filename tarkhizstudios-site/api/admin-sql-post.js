51e3ebd06d62638d74ca84190524ee7a420acf70
Igor Torres <igortorres234@gmail.com>
2026-08-16 20:53:37 -0300

    Correção nas variaveis de ambiente supabaseclient.js

--- tarkhizstudios-site/api/admin-sql-post.js
+++ tarkhizstudios-site/api/admin-sql-post.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
+const supabaseUrl = process.env.VITE_SUPABASE_URL;
+const supabaseServiceKey = process.env.VITE_SUPABASE_SERVICE_ROLE_KEY;
 
 export default async function handler(req, res) {
   // 1. Configuração de CORS
