aa9702fb57d772722ab571d2fc2128ade22574ae
Igor Torres <45331732+IG0RT0RRES@users.noreply.github.com>
2026-09-04 10:46:35 -0300

    Atualizar o panel_adm.jsx

--- tarkhizstudios-site/src/components/panel_adm.jsx
+++ tarkhizstudios-site/src/components/panel_adm.jsx
@@ -1,1 +1,9 @@
 #Criação de page
+
+import React from 'react';
+import { ExternalLink, Github, Code2, Smartphone, Globe } from 'lucide-react';
+
+export default function panel_adm() {
+
+   return {}
+}
